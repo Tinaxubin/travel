6a444b4f393163d3bcbc784dd43e3628ec8bbf23
tina <tinaxu@saas.top>
2019-10-25 22:00:31 +0800

    swiper-update

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'  //swiper组件
 import 'style/index.css'
+import 'swiper/dist/css/swiper.css'
 // 解决移动端点击延迟问题
 import fastclick from 'fastclick'
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
+
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="swiper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide>
+        <img class="swiper-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572021318012&di=072f59b39902993a041624aeff868fe9&imgtype=0&src=http%3A%2F%2Fp4-q.mafengwo.net%2Fs9%2FM00%2FBC%2F91%2FwKgBs1b7e9SANgXsADNR-a5p1tw97.jpeg"/>
+      </swiper-slide>
+      <swiper-slide>
+        <img class="swiper-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572021497104&di=152bcbb802e0ceae4dfdd698504d9051&imgtype=jpg&src=http%3A%2F%2Fimg3.imgtn.bdimg.com%2Fit%2Fu%3D2055447958%2C3298526593%26fm%3D214%26gp%3D0.jpg"/>
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+      <!-- 左右箭头 -->
+      <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div> -->
+      <!-- 下面滚动条 -->
+      <!-- <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "homeSwiper",
+    data() {
+      return{
+        swiperOption: {}
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .swiper
+    width: 100%;
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 31.25%;
+    background-color: #ccc;
+    .swiper-img
+      width: 100%;
+</style>
